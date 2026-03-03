b10ef6ec4499dd937b2dd6dd1c4c212e6c1bdadb
laurent7850 <divers@distr-action.com>
2026-03-03 23:06:01 +0100

    fix: include pdfkit font data files for PDF report generation

--- Autoseo/next.config.js
+++ Autoseo/next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     instrumentationHook: true,
+    outputFileTracingIncludes: {
+      '/api/reports/pdf': ['./node_modules/pdfkit/js/data/**/*'],
+    },
   },
+  serverExternalPackages: ['pdfkit'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
